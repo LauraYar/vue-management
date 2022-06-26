9a18c1034aff7798f925fd5b97e12bb74e61fe9d
LauraYar <807937406@qq.com>
2022-06-26 16:03:33 +0800

    完善左侧菜单栏功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,144 @@
 <template>
-  <div>
-    Home页面
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部导航 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <!-- 侧边栏菜单区域 -->
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          default-active="2"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#409BFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体区域 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao',
+      },
+      isCollapse: false,
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
   methods: {
     logout() {
       window.sessionStorage.clear();
       this.$router.push('/login');
     },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus');
+      if (res.meta.status != 200) return this.$message.error(res.meta.msg);
+      this.menulist = res.data;
+      console.log(res);
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
+    },
   },
 };
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  height: 100%;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  background: #373d41;
+  color: #fff;
+  font-size: 18px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 16px;
+    }
+  }
+}
+.el-aside {
+  background: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+// 点击按钮实现菜单的折叠与展开
+</style>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,29 @@ import store from './store';
 import './assets/css/common.css';
 import './assets/fonts/iconfont.css';
 import 'element-ui/lib/theme-chalk/index.css';
-import { Button, Form, FormItem, Input, Container, Message } from 'element-ui';
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Container,
+  Message,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+} from 'element-ui';
 import axios from 'axios';
 // 配置请求的根路径
 axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1';
+axios.interceptors.request.use((config) => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+});
 Vue.prototype.$http = axios;
 
 Vue.use(Button);
@@ -16,6 +35,13 @@ Vue.use(Form);
 Vue.use(FormItem);
 Vue.use(Input);
 Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
 // Vue.use(Message);message需要进行全局挂载，跟其他不一样
 Vue.prototype.$message = Message;
 Vue.config.productionTip = false;
